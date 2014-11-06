1b0e6da8a9ccd40cb34d0859cbbd65e991673b0b
Fernando Crespo <fernando82@gmail.com>
2014-11-06 20:00:35 -0200

    Some fixed to python to follow PEP8

--- bootstrap.py
+++ bootstrap.py
@@ -8,26 +8,27 @@ Options:
     install     Install dotfiles
     update      Update dotfiles
     remove      Removes dotfiles restoring your backups (if they exist)
-    -d          Test with the info passed to make sure is correct makes backup but do not override originals
+    -d          Test with the info passed to make sure is correct makes backup
+                but do not override originals
     -h          This help
     -force_ext  rbenv, pyenv, powerline-shell, all
 '''
 from __future__ import print_function
 from __future__ import unicode_literals
 import os
-import re
-import sys
+#import re
+#import sys
 import shutil
-import subprocess
+#import subprocess
 import logging
 try:
     import coloredlogs
-except:
+except Exception:
     logging.warning('Please install coloredlogs - pip install coloredlogs')
 
 try:
     from docopt import docopt
-except:
+except Exception:
     logging.critical('Please install docopt - pip install docopt')
     exit(1)
 
@@ -35,28 +36,30 @@ __version__ = '1.0'
 
 class MyInfo(object):
     """docstring for MyInfo"""
+    os_name = 'none'
     def __init__(self):
-        import re
-        import os
+        #import re
+        #import os
         import sys
-        import logging
+        #import logging
         super(MyInfo, self).__init__()
 
         if 'linux'.lower() in map(str.lower, os.uname()):
-            self.os = 'linux'
+            self.os_name = 'linux'
         elif 'darwin'.lower() in map(str.lower, os.uname()):
-            self.os = 'mac'
+            self.os_name = 'mac'
         else:
             logging.critical('Cannot determine what SO you are running')
 
         self.script_dir = os.path.realpath(os.path.dirname(sys.argv[0]))
         self.home = os.environ['HOME']
-        self.files = ['.bashrc', '.bash_profile', '.gitconfig', '.hushlogin', '.vimrc']
+        self.files = ['.bashrc', '.bash_profile', '.gitconfig', '.hushlogin',
+                      '.vimrc']
 
-        # Verificar se tem um melhor modo de tratar especificidades de plataforma
-        if 'linux' in self.os:
+        #Verificar se tem um melhor modo de tratar especificidades de plataforma
+        if 'linux' in self.os_name:
             self.files.extend(['linux.sh'])
-        if 'mac' in self.os:
+        if 'mac' in self.os_name:
             self.files.extend(['mac.sh'])
 
         self.debug = False
@@ -69,17 +72,27 @@ def debug(info):
     for _file in info.files:
         _src = os.path.join(info.home, _file)
         _dst = os.path.join(info.script_dir, _file)
-        isFile = 'file' if os.path.isfile(_src) else 'link destination do not exist'
-        isLink = 'link' if os.path.islink(_src) else 'do not exist'
-        text += '\n{0} ({2})\n\t-> {1}'.format(_src, _dst, ','.join([isLink, isFile]))
+        if os.path.isfile(_src):
+            is_file = 'file'
+        else:
+            is_file = 'link destination do not exist'
+
+        if os.path.islink(_src):
+            is_link = 'link'
+        else:
+            is_link = 'do not exist'
+        text += '\n{0} ({2})\n\t-> {1}'.format(
+            _src, _dst, ','.join([is_link, is_file]))
     text += '\n{:<20}{:<20}'.format('Home dir:', info.home)
     logging.debug(text)
 
 def backup(info):
     text = 'Backing up files'
     from datetime import datetime
-    _backup_dir = os.path.join(info.script_dir, 'backup-{}'.format(datetime.strftime(datetime.now(), '%Y.%m.%d-%H.%M.%S')))
-    if not os.path.exists(_backup_dir): os.mkdir(_backup_dir)
+    _backup_dir = os.path.join(info.script_dir, 'backup-{}'.format(
+        datetime.strftime(datetime.now(), '%Y.%m.%d-%H.%M.%S')))
+    if not os.path.exists(_backup_dir):
+        os.mkdir(_backup_dir)
     with open(os.path.join(_backup_dir, 'backuplist.log'), 'w') as _backup_file:
         for _file in info.files:
             _src = os.path.realpath(os.path.join(info.home, _file)).strip()
@@ -102,11 +115,10 @@ def restore(info):
             if os.path.exists(_dst):
                 _src = os.path.join(_backup_dir, _file).strip()
                 logging.info('Restoring file {}'.format(_file))
-                if not info['debug']: shutil.copy(_src, _dst)
+                if not info['debug']:
+                    shutil.copy(_src, _dst)
 
-def install(info, update=False):
-    aborted = False
-
+def install(info):
     backup(info)
 
     text = 'Creating symlinks'
@@ -116,8 +128,10 @@ def install(info, update=False):
         _src_log = os.path.join('<DOTFILES_DIR>', _file)
         _dst_log = os.path.join('~', _file)
         text += '\n{:<40} -> {:<30}'.format(_src_log, _dst_log)
-        if not info.debug and os.path.exists(_dst): os.remove(_dst)
-        if not info.debug: os.symlink(_src, _dst)
+        if not info.debug and os.path.exists(_dst):
+            os.remove(_dst)
+        if not info.debug:
+            os.symlink(_src, _dst)
 
     logging.info(text)
 
@@ -125,7 +139,9 @@ def install(info, update=False):
 run \'source ~/.bash_profile\' or open another terminal to see the changes.
 If you are updating an existing installation you can run \'update-dotfiles\'''')
 
-def choose(msg='Do you want to delete your config file?', choices={ 'y': True, 'n': False }, default='n'):
+def choose(msg='Do you want to delete your config file?',
+           choices={'y': True, 'n': False},
+           default='n'):
     _choice = ''
     while _choice.lower() not in choices.keys():
         try:
@@ -134,7 +150,7 @@ def choose(msg='Do you want to delete your config file?', choices={ 'y': True, '
             _choice = unicode(raw_input('{} ({}): '.format(msg, _choices).encode('utf-8')))
             _choice = default if _choice == '' else _choice
             _choosen = choices[_choice[0].lower()]
-        except Exception, e:
+        except Exception:
             _choice = ''
     return _choosen
 
@@ -145,7 +161,8 @@ def remove(info):
         _answer = True
         if os.path.exists(_src) and os.path.islink(_src) and _answer:
             print('Removing symlink to {}'.format(_file))
-            if not info.debug and os.path.exists(_src): os.remove(_src)
+            if not info.debug and os.path.exists(_src):
+                os.remove(_src)
 
     restore(info)
 
@@ -172,15 +189,17 @@ def main():
     _info = MyInfo()
     args = docopt(__doc__)
 
-    if args.has_key('-d') and args['-d'] or (args.has_key('test') and args['test']):
+    if (args.has_key('-d') and args['-d']) or (args.has_key('test')
+                                               and args['test']):
         _info.debug = True
         _info.log_level = logging.DEBUG
     else:
         _info.log_level = logging.INFO
 
     try:
-        coloredlogs.install(show_hostname=False, show_name=False, level=_info.log_level)
-    except:
+        coloredlogs.install(show_hostname=False,
+                            show_name=False, level=_info.log_level)
+    except Exception:
         logging.basicConfig(level=_info.log_level)
 
     logging.debug('Passed arguments: {}'.format(args))
@@ -191,7 +210,7 @@ def main():
     if args.has_key('install') and args['install']:
         install(_info)
     elif args.has_key('update') and args['update']:
-        install(_info, args['update'])
+        install(_info)
     elif args.has_key('remove') and args['remove']:
         remove(_info)
     elif args.has_key('test') and args['test']:

f0689bc6c1d5e675cb689c7d8b163310edddef2a
Fernando Crespo <fernando82@gmail.com>
2016-02-11 19:13:16 -0200

    Removed git clone completion in favor of hub completion, added hub to bash_profile, removed subl from mac

--- bash_profile.sh
+++ bash_profile.sh
@@ -117,6 +117,7 @@ if [ -x "$(which git 2> /dev/null)" ]; then
     alias gp='git push'
     alias gd="git diff"
     alias grmall='gs | grep deleted | cut -c 15- | xargs -i* git rm "*"'
+    eval "$(hub alias -s)"
 fi
 
 alias update-dotfiles='source ~/.bash_profile'

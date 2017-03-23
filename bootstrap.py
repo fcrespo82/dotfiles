55e1ff5e27e4e88e1724284ac88a12c283fec0c0
Fernando Crespo <fernando82@gmail.com>
2017-03-23 00:59:21 -0300

    Updates

--- bootstrap.py
+++ bootstrap.py
@@ -1,54 +1,42 @@
 #!.venv/bin/python
 '''usage:
-    bootstrap.py [-d]
+    bootstrap.py [-d] (install [--no-backup]|remove)
+    bootstrap.py -h|--help
 
 options:
-    -d          Enable debug
-
+    -d              Enable debug
+    install         Install dotfiles
+    --no-backup     Do NOT Backup files before install
+    remove          Remove dotfiles
+    -h|--help       Show this help
 '''
-from pathlib import Path
-from distutils.spawn import find_executable
 import logging
+from lib import Config, Installation
 import yaml
 from docopt import docopt
+import coloredlogs
 
 VERSION = '1.0'
 
 
-def verify_app(app):
-    if app['type'] == 'folder':
-        #pylint: disable=no-member
-        return Path(app[app['type']]).expanduser().exists()
-    else:
-        if find_executable(app[app['type']]):
-            return True
-    return False
-
-
-def verify_requirements(apps):
-    logging.info('Checking for requirements')
-    for app in apps:
-        installed = verify_app(apps[app])
-        apps[app]['installed'] = installed
-        logging.debug('%s - installed: %s', app, installed)
-
-    for app in apps:
-        if not apps[app]['installed'] and apps[app]['required']:
-            logging.fatal('\'%s\' is required but is not installed', app)
-            return False
-    return True
-
-
 def main():
-    logging.basicConfig(level=LOG_LEVEL)
-    apps = yaml.load(open('apps.yaml'))
-    ok = verify_requirements(apps)
-    print(ok)
+    _log = logging.getLogger()
+    apps = yaml.load(open('apps.yml'))
+    files = yaml.load(open('files.yml'))
+
+    _log.info('Loading config')
+    cfg = Config(apps, files, PARAMS['install'], PARAMS['--no-backup'])
+    inst = Installation(cfg)
+
+    inst.perform()
 
 
 if __name__ == '__main__':
     PARAMS = docopt(__doc__, version=VERSION)
+    print(PARAMS)
     LOG_LEVEL = logging.INFO
     if PARAMS['-d']:
         LOG_LEVEL = logging.DEBUG
+    coloredlogs.install(level=LOG_LEVEL)
+    logging.basicConfig(level=LOG_LEVEL)
     main()

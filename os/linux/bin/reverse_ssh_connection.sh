7823d459edabcc05eda582a95cd8dbe0ccb7e5af
Fernando Crespo <fernando82@gmail.com>
2018-10-31 17:09:08 -0300

    Removed old code

--- os/linux/bin/reverse_ssh_connection.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-echo "Starting remote ssh port redirect from nas.crepo.in:1234 to localhost:22"
-until `ssh -p 6080 fernando@nas.crespo.in -R 1234:localhost:22 -N`; do
-    echo "Restarting remote ssh port redirect from nas.crepo.in:1234 to localhost:22"
-    sleep 1
-done
--- os/linux/bin/reverse_ssh_connection_nas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-echo "Starting remote ssh port redirect from nas:1234 to localhost:22"
-until `ssh -p 3456 root@nas.crespo.in -R 1234:localhost:22 -N`; do
-    echo "Restarting remote ssh port redirect from nas:1234 to localhost:22"
-    sleep 1
-done

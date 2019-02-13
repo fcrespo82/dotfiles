a69c7bf594b1e019f6467fe5127a168214fe6578
Fernando Crespo <fernando82@gmail.com>
2019-02-13 18:05:21 -0200

    Added beacon code

---
+++ macOS/LaunchDaemons/beacon.sh
@@ -0,0 +1,24 @@
+source $DOTFILES_DIR/utils
+
+ensure_mbeacon() {
+	if [ -z $(command -v mbeacon) ]; then
+		echo "Installing mbeacon"
+		case $(uname -s) in
+			Darwin)
+				ensure_brew()
+				brew tap watr/formulae && brew install mbeacon
+				;;
+		esac
+	fi
+
+start_beacon() {
+	case $(uname -s) in
+		Darwin)
+			ensure_mbeacon()
+			uuid=$(system_profiler SPHardwareDataType | awk '/UUID/ { print $3; }')
+			mbeacon --uuid $uuid
+			;;
+	esac
+}
+
+start_beacon()

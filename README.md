672e53ee5484da41e0f0c22874c28d19147a8cb5
Fernando Crespo <fernando82@gmail.com>
2024-05-02 23:55:50 -0300

    Improving instalation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Install instructions
 
 ```shell
-git clone --depth=1 https://github.com/fcrespo82/dotfiles $HOME/dotfiles && cd $HOME/dotfiles && ./install.zsh
+git clone --depth=1 git@github.com:fcrespo82/dotfiles.git $HOME/dotfiles && cd $HOME/dotfiles && ./install.zsh
 ```
--- install.zsh
+++ install.zsh
@@ -2,12 +2,15 @@
 
 
 # Install dependencies first?
-# sudo pacman -Sy --needed git base-devel coreutils
-# git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-# yay -Sy --noconfirm bat
-# yay -Sy --noconfirm eza
-# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
-# git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.14.0
-
+sudo pacman -Sy --needed git base-devel coreutils
+# Test if is installed before cloning
+git clone https://aur.archlinux.org/yay.git /tmp/yay && cd /tmp/yay && makepkg -si
+yay -Sy --noconfirm bat eza stow
+# if [ ! -d "$HOME/powerlevel10k" ]; then
+#     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
+# fi
+if [ ! -d "$HOME/.asdf" ]; then
+    git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.14.0
+fi
 
 stow --dotfiles .

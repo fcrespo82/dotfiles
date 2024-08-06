a463d02314a3d007d1afb5717f5fb9555ae56646
Fernando Crespo <fernando82@gmail.com>
2024-08-06 11:28:56 -0300

    Improved multi setup behaviour

---
+++ home/fernando/PACKAGES.sh
@@ -0,0 +1,13 @@
+export USER_PACKAGES=(
+    1password
+    apple-fonts
+    bat
+    chromedriver
+    eza
+    google-chrome
+    stow
+    trash-cli
+    visual-studio-code-bin
+    zsh-autosuggestions
+    konsole-dracula-git
+)
--- install.zsh
+++ install.zsh
@@ -1,68 +1,7 @@
+DRY_RUN=${DRY_RUN:-true}
 
-
-SYSTEM_PACKAGES=(
-    ark
-    base
-    base-devel
-    btrfs-progs
-    dkms
-    dolphin
-    efibootmgr
-    egl-wayland
-    fzf
-    git
-    gst-plugin-pipewire
-    htop
-    intel-ucode
-    iwd
-    kate
-    konsole
-    less
-    lib32-nvidia-utils
-    libpulse
-    linux
-    linux-firmware
-    linux-headers
-    man-db
-    man-pages
-    nano
-    network-manager-applet
-    networkmanager
-    nvidia-dkms
-    pipewire
-    pipewire-alsa
-    pipewire-jack
-    pipewire-pulse
-    plasma-meta
-    plasma-workspace
-    plymouth
-    smartmontools
-    sof-firmware
-    stow
-    vim
-    wget
-    wireless_tools
-    wireplumber
-    xdg-utils
-    xorg-xeyes
-    xorg-xinit
-    zram-generator
-    zsh
-)
-
-USER_PACKAGES=(
-    1password
-    apple-fonts
-    bat
-    chromedriver
-    eza
-    google-chrome
-    stow
-    trash-cli
-    visual-studio-code-bin
-    zsh-autosuggestions
-    konsole-dracula-git
-)
+user=$(find home -mindepth 1 -maxdepth 1 -type d -printf '%f\n' | fzf)
+host=$(find hosts -mindepth 2 -maxdepth 2 -type d -printf '%P\n' | fzf)
 
 PACMAN_FLAGS=(
     --needed
@@ -70,36 +9,80 @@ PACMAN_FLAGS=(
     --noconfirm
 )
 
-# Install dependencies first?
-sudo pacman -Sy ${PACMAN_FLAGS[@]} ${SYSTEM_PACKAGES[@]}
+source home/$user/PACKAGES.sh
+source hosts/$host/PACKAGES.sh
 
+if [ "$DRY_RUN" = true ]; then
+    echo "================================================================================"
+    echo "=                                   DRY RUN                                    ="
+    echo "================================================================================"
+    echo
+    echo "Installing on $user@$host"
+    echo
+    echo "User packages: ${USER_PACKAGES[@]}"
+    echo
+    echo "System packages: ${SYSTEM_PACKAGES[@]}"
+    echo
+fi
 
-if ! command -v yay >/dev/null 2>&1; then
-    # Test if is installed before cloning
-    git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin && cd /tmp/yay-bin && makepkg -si
+if [ "$DRY_RUN" = false ]; then
+    sh home/$user/hooks/pre-install
 else
-    yay -Sy ${PACMAN_FLAGS[@]} ${USER_PACKAGES[@]}
+    echo sh home/$user/hooks/pre-install
+    echo
 fi
 
-
-if [ ! -d "$HOME/.config/zsh/themes/powerlevel10k" ]; then
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.config/zsh/themes/powerlevel10k
+if [ "$DRY_RUN" = false ]; then
+    sudo pacman -Sy ${PACMAN_FLAGS[@]} ${SYSTEM_PACKAGES[@]}
+else
+    echo sudo pacman -Sy ${PACMAN_FLAGS[@]} ${SYSTEM_PACKAGES[@]}
+    echo
 fi
+
+if [ "$DRY_RUN" = false ]; then
+    if ! command -v yay >/dev/null 2>&1; then
+        git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin && cd /tmp/yay-bin && makepkg -si
+    else
+        yay -Sy ${PACMAN_FLAGS[@]} ${USER_PACKAGES[@]}
+    fi
+else
+    echo yay -Sy ${PACMAN_FLAGS[@]} ${USER_PACKAGES[@]}
+    echo
+fi
+
+if [ "$DRY_RUN" = false ]; then
+    if [ ! -d "$HOME/.config/zsh/themes/powerlevel10k" ]; then
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.config/zsh/themes/powerlevel10k
+    fi
+else
+    echo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.config/zsh/themes/powerlevel10k
+    echo
+fi
+
 # if [ ! -d "$HOME/.asdf" ]; then
 #     git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.14.0
 # fi
 
 
-find home -mindepth 1 -maxdepth 1 -type d -not -name '.*' -printf '%f\n' \
-| fzf  --multi --preview-window up:80% --preview 'stow -n -vv --dotfiles --dir=home --target=$HOME {}' \
-| xargs -ro stow -vv --dotfiles --dir=home --target=$HOME
+if [ "$DRY_RUN" = false ]; then
+    stow -vv --dotfiles --dir=home --target=$HOME $user
+else
+    echo stow -vv --dotfiles --dir=home --target=$HOME $user
+    echo
+fi
+
+# find hosts -mindepth 2 -maxdepth 2 -type d -not -name '.*' -printf '%f\n' \
+# | fzf --preview-window up:80% --preview 'stow -n -vv --dir=hosts/personal --target=/ {}' \
+# | xargs -ro sudo stow -vv --dir=hosts/personal --target=/
 
 
-find hosts -mindepth 2 -maxdepth 2 -type d -not -name '.*' -printf '%f\n' \
-| fzf  --multi --preview-window up:80% --preview 'stow -n -vv --dir=hosts/personal --target=/ {}' \
-| xargs -ro sudo stow -vv --dir=hosts/personal --target=/
+if [ "$DRY_RUN" = false ]; then
+    sh home/$user/hooks/post-install
+else
+    echo sh home/$user/hooks/post-install
+    echo
+fi
 
-sudo systemd-hwdb update
-sudo udevadm trigger
-
-exec zsh
+if [ "$DRY_RUN" = false ]; then
+    exec zsh
+fi

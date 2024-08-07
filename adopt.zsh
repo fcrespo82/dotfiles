f9205f0db7ecd6347fc6d72b01b54280c8c63cf0
Fernando Crespo <fernando82@gmail.com>
2024-08-07 08:35:34 -0300

    Improve adopt

--- adopt.zsh
+++ adopt.zsh
@@ -1,9 +1,9 @@
 
 find home -mindepth 1 -maxdepth 1 -type d -not -name '.*' -printf '%f\n' \
-| fzf  --multi --preview-window up:80% --preview 'stow -n -vv --adopt --dotfiles --dir=home --target=$HOME {}' \
+| fzf --preview-window up:80% --preview 'stow -n -vv --adopt --dotfiles --dir=home --target=$HOME {}' \
 | xargs -ro stow -vv --adopt --dotfiles --dir=home --target=$HOME
 
 
-find hosts -mindepth 2 -maxdepth 2 -type d -not -name '.*' -printf '%f\n' \
-| fzf  --multi --preview-window up:80% --preview 'stow -n -vv --adopt --dir=hosts/personal --target=/ {}' \
-| xargs -ro sudo stow -vv --adopt --dir=hosts/personal --target=/
+find hosts -mindepth 2 -maxdepth 2 -type d -printf '%P\n' \
+| fzf --preview-window up:80% --preview 'stow -n -vv --adopt --dir=hosts/{} --target=/ .' \
+| xargs -ro sudo stow -vv --adopt --dir=hosts/{} --target=/ .
